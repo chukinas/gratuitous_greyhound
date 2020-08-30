4b0bcca48a15fb9b9c40e080b36efa399edd4850
Jonathan Chukinas (home) <chukinas@gmail.com>
2020-08-30 19:03:25 -0400

    plug: sanitize room name

--- lib/chukinas_web/live/chat_live.ex
+++ lib/chukinas_web/live/chat_live.ex
@@ -1,6 +1,6 @@
 defmodule ChukinasWeb.ChatLive do
   use ChukinasWeb, :live_view
-  # import Phoenix.HTML.Link
+
   #############################################################################
   # CALLBACKS
   #############################################################################
@@ -12,39 +12,29 @@ defmodule ChukinasWeb.ChatLive do
     {:ok, socket}
   end
 
-  # @impl true
-  # def handle_event("uuid", uuid, socket) do
-  #   IO.puts(uuid)
-  #   {:noreply, assign(socket, uuid: uuid)}
-  # end
-
   @impl true
   def handle_event("go_to_room", %{"room_name" => room_name}, socket) do
-    # TODO downcase & slugify
-    room_name = slugify(room_name)
-    socket = assign(socket, :room_name, room_name)
     {:noreply, redirect(socket, to: "/chat/"<>room_name)}
   end
 
-  #############################################################################
-  # HELPERS
-  #############################################################################
-
-  def slugify(string) do
-    string = string
-    |> String.downcase()
-    |> String.replace(~r/[^\w-]+/u, "-")
-    IO.puts(string)
-    string
+  @impl true
+  def handle_event("go_to_lobby", _params, socket) do
+    {:noreply, redirect(socket, to: "/chat")}
   end
+
 end
 
 # TODO JJC
-# git
-# go back to lobby
+# use helpers instead of hard coding the links
+#   lookup docs on live_path
 # ensure links aren't establishing new socket
 # formatter for templates
 # remove the vscode scroll thumbnail
 # remove keyboard layout shortcut
 # formatting for vscode ~L sigil
 # TODO aren't showing up highlighted
+# TODO room name as value, not placeholder
+# Where's the right place to put the plug?
+# scope the plug to just one and chat
+# should room_name be temp assigns?
+# events: go to room or lobby: use helpers
--- lib/chukinas_web/router.ex
+++ lib/chukinas_web/router.ex
@@ -8,6 +8,7 @@ defmodule ChukinasWeb.Router do
     plug :put_root_layout, {ChukinasWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug ChukinasWeb.Plugs.SanitizeRoomName
   end
 
   pipeline :api do
