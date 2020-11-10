d67f47e54f6cc10f7c91ee2245a2a759a21c1bb3
Jonathan Chukinas (home) <chukinas@gmail.com>
2020-11-09 21:32:44 -0500

    my first component - `spaces`

---
+++ lib/chukinas_web/views/skies_view.ex
@@ -0,0 +1,6 @@
+defmodule ChukinasWeb.SkiesView do
+  use ChukinasWeb, :view
+end
+
+# https://bernheisel.com/blog/phoenix-liveview-and-views
+# https://fullstackphoenix.com/tutorials/nested-templates-and-layouts-in-phoenix-framework
