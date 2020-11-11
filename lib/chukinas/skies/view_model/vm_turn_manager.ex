5e313b220487beeb1ed64b707de3370c7ec808bc
Jonathan Chukinas (home) <chukinas@gmail.com>
2020-11-11 18:35:55 -0500

    finally got the `TurnManager` view model working

--- lib/chukinas/skies/view_model/vm_turn_manager.ex
+++ lib/chukinas/skies/view_model/vm_turn_manager.ex
@@ -1,22 +1,27 @@
 defmodule Chukinas.Skies.ViewModel.TurnManager do
   alias Chukinas.Skies.Game.TurnManager
 
-  def render(turn_mgr) do
-    # Map.get_and_update!(turn_mgr, :phase, &phase_to_string/1)
-    turn_mgr
+  @type t :: %{
+    turn: integer(),
+    max_turn: integer(),
+    phase: String.t(),
+  }
+
+  @spec build(TurnManager.t()) :: t()
+  def build(turn_mgr) do
+    %{
+      turn: turn_mgr.turn,
+      max_turn: turn_mgr.max_turn,
+      phase: phase_to_string(turn_mgr.phase)
+    }
   end
 
-  # @spec build(TurnManager.t()) :: TurnManager.t()
-  # def build(turn_mgr) do
-  #   turn_mgr
-  # end
-
-  # @spec phase_to_string(TurnManager.phase()) :: String.t
-  # defp phase_to_string(phase) when is_atom(phase) do
-  #   phase
-  #   |> Atom.to_string()
-  #   |> String.capitalize()
-  # end
+  @spec phase_to_string(TurnManager.phase_name()) :: String.t
+  defp phase_to_string(phase) when is_atom(phase) do
+    phase
+    |> Atom.to_string()
+    |> String.capitalize()
+  end
 
   # defp phase_to_string(phase) do
   #   [main_phase, sub_phase] = phase
