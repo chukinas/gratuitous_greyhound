be6bfd051fd7d9930c0c279ead5c13d05d391140
Jonathan Chukinas (home) <chukinas@gmail.com>
2020-11-10 22:42:30 -0500

    new `position.html.leex`

--- lib/chukinas_web/views/skies_view.ex
+++ lib/chukinas_web/views/skies_view.ex
@@ -1,16 +1,20 @@
 defmodule ChukinasWeb.SkiesView do
   use ChukinasWeb, :view
-  # use ChukinasWeb, :live_view
-  
+
   def build_component_renderer(vm) do
     fn view_model_key -> render_component(vm, view_model_key) end
+  end
+
+  # TODO some day, this should be private
+  def render_component(key) do
+    build Atom.to_string(key)
   end
 
   def render_component(view_model, key) do
     build Atom.to_string(key), Map.fetch!(view_model, key)
   end
 
-  defp build(name, assigns) do
+  defp build(name, assigns \\ []) do
     template = [name, ".html"] |> Enum.join("")
     Phoenix.View.render(__MODULE__, template, assigns)
   end
