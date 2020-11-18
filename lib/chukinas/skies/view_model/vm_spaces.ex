bad9e4e95c28d6734d4564ed2d5c3dad13575a81
Jonathan Chukinas (home) <chukinas@gmail.com>
2020-11-18 11:23:02 -0500

    VM Space struct

--- lib/chukinas/skies/view_model/vm_spaces.ex
+++ lib/chukinas/skies/view_model/vm_spaces.ex
@@ -1,13 +1,23 @@
 defmodule Chukinas.Skies.ViewModel.Spaces do
+  alias Chukinas.Skies.ViewModel.Space
 
+  @type t :: [Space.t()]
+
+  # TODO rename 'build'
   def render(spaces) do
     spaces
     |> Map.to_list()
-    |> Enum.map(&render_space/1)
+    |> Enum.map(&Space.build/1)
   end
+end
 
-  defp render_space({{x, y}, content}) do
-    %{x: x, y: y, lethal_level: content}
+defmodule Chukinas.Skies.ViewModel.Space do
+  defstruct [:x, :y, :lethal_level]
+  @type t :: %__MODULE__{}
+  @type coordinates :: {integer(), integer()}
+
+  @spec build({coordinates(), any()}) :: t()
+  def build({{x, y}, content}) do
+    %__MODULE__{x: x, y: y, lethal_level: content}
   end
-
 end
