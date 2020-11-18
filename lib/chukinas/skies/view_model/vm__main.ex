37d8fd43a3339540dd5335a7593f612894807f64
Jonathan Chukinas (home) <chukinas@gmail.com>
2020-11-18 11:41:16 -0500

    Add VM struct

--- lib/chukinas/skies/view_model/vm__main.ex
+++ lib/chukinas/skies/view_model/vm__main.ex
@@ -1,8 +1,18 @@
 defmodule Chukinas.Skies.ViewModel do
   alias Chukinas.Skies.ViewModel.{Boxes, Squadron, Spaces, TurnManager}
 
+  defstruct [
+    :squadron,
+    :spaces,
+    :bombers,
+    :boxes,
+    :turn_manager,
+    :escort_stations,
+  ]
+
+  # TODO rename 'build'
   def render(game) do
-    %{
+    %__MODULE__{
       squadron: Squadron.build(game.squadron),
       spaces: Spaces.render(game.spaces),
       bombers: render_bombers(game.elements),
@@ -12,6 +22,7 @@ defmodule Chukinas.Skies.ViewModel do
     }
   end
 
+  # TODO move this elsewhere
   defp render_bombers(elements) do
     Enum.concat(elements)
   end
