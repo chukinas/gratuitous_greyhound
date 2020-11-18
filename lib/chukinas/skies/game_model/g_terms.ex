c7940484bf8344bf28a2a903bc68702a5327100b
Jonathan Chukinas (home) <chukinas@gmail.com>
2020-11-18 13:02:34 -0500

    fix dialyzer warnings

--- lib/chukinas/skies/game_model/g_terms.ex
+++ lib/chukinas/skies/game_model/g_terms.ex
@@ -1,8 +1,9 @@
 defmodule Chukinas.Skies.Terms do
-  @type generic_direction :: :nose, :left, :flank
-  @type specific_direction :: :nose, :left, :right, :tail
+  # TODO rename Chukinas.Skies.Positions? Or combine into that module?
+  @type generic_direction :: :nose | :tail | :flank
+  @type specific_direction :: :nose | :tail | :left | :right
 
-  @spec to_generic(specific_direction) :: generic_direction()
+  @spec to_generic(specific_direction()) :: generic_direction()
   def to_generic(direction) do
     case direction do
       :right -> :flank
