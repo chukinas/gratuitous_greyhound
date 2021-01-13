0f6ae00af946e3ea62121604ed9e1ceb1385a187
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-01-12 23:21:08 -0500

    render a minimal card template

--- lib/chukinas_web/live/dreadnought_live.ex
+++ lib/chukinas_web/live/dreadnought_live.ex
@@ -1,12 +1,14 @@
 defmodule ChukinasWeb.DreadnoughtLive do
   use ChukinasWeb, :live_view
   alias Chukinas.Dreadnought.Model.Deck
+  alias ChukinasWeb.DreadnoughtView
 
   @impl true
   def mount(_params, _session, socket) do
     socket = socket
     |> assign(page_title: "Dreadnought")
     |> assign(deck: Deck.new(1) |> Deck.draw(5))
+    # TODO remove:
     |> assign(angles: ~w"000 030 045 060 090 180")
     {:ok, socket}
   end
---
+++ lib/chukinas_web/views/dreadnought.ex
@@ -0,0 +1,7 @@
+defmodule ChukinasWeb.DreadnoughtView do
+  use ChukinasWeb, :view
+end
+
+# https://bernheisel.com/blog/phoenix-liveview-and-views
+# https://www.wyeworks.com/blog/2020/03/03/breaking-up-a-phoenix-live-view/
+# https://fullstackphoenix.com/tutorials/nested-templates-and-layouts-in-phoenix-framework
