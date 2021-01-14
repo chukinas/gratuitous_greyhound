b54a7e2c0b4c66fc7a89baf7dde47e724543e863
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-01-13 21:04:47 -0500

    Move the Card arrow svg paths to helper functions
    These functions are static for now - they return a 90deg shaft and and
    60deg head. Following commits will see the path corrected and
    dynamically generated.

--- lib/chukinas_web/views/dreadnought.ex
+++ lib/chukinas_web/views/dreadnought.ex
@@ -1,5 +1,13 @@
 defmodule ChukinasWeb.DreadnoughtView do
   use ChukinasWeb, :view
+
+  def build_svg_arrow_shaft(angle) do
+    "m 3 10 q 0 -4, 4 -4"
+  end
+
+  def build_svg_arrow_head(angle) do
+    "m 6.8 3.7 0.83 -1.6 -1.9 -0.17 0.89 0.71 z"
+  end
 end
 
 # https://bernheisel.com/blog/phoenix-liveview-and-views
