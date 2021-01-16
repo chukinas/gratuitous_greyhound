5e7f971eedf7f5192e9e39b9eccff151b74e2178
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-01-16 17:01:57 -0500

    Fix dialyzer warnings for Dreadnought.Cartesian

--- lib/chukinas/dreadnought/cartesian.ex
+++ lib/chukinas/dreadnought/cartesian.ex
@@ -1,12 +1,11 @@
 defmodule Chukinas.Dreadnought.Cartesian do
 
-  @type coordinate :: {integer(), integer()}
+  @type coordinate :: {number(), number()}
 
   @spec translate_coordinate_list([coordinate], coordinate) :: [coordinate]
-  def translate_coordinate_list(coords, translation) do
+  def translate_coordinate_list(coords, dxdy) do
     coords
-    |> Enum.map(&translate_coordinate(&1, translation))
-    # TODO I don't like the term translation
+    |> Enum.map(&translate_coordinate(&1, dxdy))
   end 
 
   @spec translate_coordinate(coordinate, coordinate) :: coordinate
@@ -14,14 +13,13 @@ defmodule Chukinas.Dreadnought.Cartesian do
     {x + dx, y + dy}        
   end 
 
-  # TODO angle is ok being integer. coord tho, that needs to be float
-  @spec rotate_coordinate_list(coordinate, integer) :: coordinate
+  @spec rotate_coordinate_list([coordinate], number()) :: [coordinate]
   def rotate_coordinate_list(coords, angle_deg) do
     coords
     |> Enum.map(&rotate_coordinate(&1, angle_deg))
   end 
 
-  @spec rotate_coordinate(coordinate, integer) :: coordinate
+  @spec rotate_coordinate(coordinate, number()) :: coordinate
   def rotate_coordinate({x, y}, angle_deg) do
     angle_rad = deg_to_rad(angle_deg)
     x1 = x * :math.cos(angle_rad) - y * :math.sin(angle_rad)
@@ -29,14 +27,17 @@ defmodule Chukinas.Dreadnought.Cartesian do
     {x1, y1}
   end 
 
+  @spec deg_to_rad(number()) :: number()
   def deg_to_rad(angle_deg) do
     angle_deg * :math.pi() / 180
   end
 
+  @spec mirror_x(coordinate()) :: coordinate()
   def mirror_x({x, y}) do
     {x, -y}
   end
 
+  @spec mirror_y(coordinate()) :: coordinate()
   def mirror_y({x, y}) do
     {-x, y}
   end
