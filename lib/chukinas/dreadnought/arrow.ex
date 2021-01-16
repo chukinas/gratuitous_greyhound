f28a22d675530e928c8fe3a3b91e33fd179d5ae4
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-01-16 16:40:50 -0500

    Programmatically generate Card arrow head path
    The starting location for the head is just a static placeholder value
    for now. I'll fix that shortly.

--- lib/chukinas/dreadnought/arrow.ex
+++ lib/chukinas/dreadnought/arrow.ex
@@ -2,24 +2,69 @@
 defmodule Chukinas.Dreadnought.Arrow do
   alias Chukinas.Dreadnought.Cartesian
 
+  # *** *******************************
+  # *** API
+  
   def build_arrow_svg_paths(angle_deg) do
-    %{shaft: build_svg_arrow_shaft(angle_deg), head: build_svg_arrow_head(angle_deg)}
+    end_point = {4, 4}
+    %{shaft: build_svg_arrow_shaft(angle_deg), head: build_svg_arrow_head(end_point, angle_deg)}
   end
 
-  # TODO move to a dedicated module
+  # *** *******************************
+  # *** HELPERS - SHAFT
+  
   defp build_svg_arrow_shaft(angle) do
     "m 3 10 #{build_svg_arrow_shaft_curve(angle)}"
   end
 
-  defp build_svg_arrow_head(angle) do
-    "m 6.8 3.7 0.83 -1.6 -1.9 -0.17 0.89 0.71 z"
-  end
-
-  defp build_svg_arrow_shaft_curve(angle) do
+  defp build_svg_arrow_shaft_curve(_angle) do
     # TODO change length to a module property
-    # TODO move these helpers to a new module
     path_len = 10
     "q 0 -#{path_len/2}, 4 -4"
   end
 
+  # *** *******************************
+  # *** HELPERS - HEAD
+
+  defp build_svg_arrow_head(point, angle_deg) do
+    tip = {0, -1.18}
+    barb = {1, 0.42}
+    # TODO replace above with actual values
+    # TODO make a note in the docs that y points down
+    # TODO is `points` a better term for x,y than `coordinates`?
+    # TODO make a not that angle is relative to straight up. It's a command card value. point is a view/rendering value
+    points = [
+      {0, 0},
+      barb,
+      tip,
+      Cartesian.mirror_y(barb)
+    ]
+    |> Cartesian.rotate_coordinate_list(angle_deg)
+    |> Cartesian.translate_coordinate_list(point)
+    |> to_space_separated_strings()
+    "M #{points} z"
+  end
+
+  # *** *******************************
+  # *** HELPERS
+
+  def to_space_separated_strings(list_of_points) do
+    list_of_points
+    |> Enum.map(&point_to_space_separated_string/1)
+    |> Enum.join(", ")
+  end
+
+  def point_to_space_separated_string({x, y}) do
+    "#{_round(x)} #{_round(y)}"
+  end
+
+  def _round(number) when is_integer(number) do
+    number
+  end
+  def _round(number) do
+    Float.round(number, 1)
+  end
+  
+  # TODO run dialyxir.
+  # TODO can I run dialyxir on every git commit?
 end
--- lib/chukinas/dreadnought/cartesian.ex
+++ lib/chukinas/dreadnought/cartesian.ex
@@ -5,7 +5,8 @@ defmodule Chukinas.Dreadnought.Cartesian do
   @spec translate_coordinate_list([coordinate], coordinate) :: [coordinate]
   def translate_coordinate_list(coords, translation) do
     coords
-    |> Enum.map(translation)
+    |> Enum.map(&translate_coordinate(&1, translation))
+    # TODO I don't like the term translation
   end 
 
   @spec translate_coordinate(coordinate, coordinate) :: coordinate
@@ -13,8 +14,15 @@ defmodule Chukinas.Dreadnought.Cartesian do
     {x + dx, y + dy}        
   end 
 
+  # TODO angle is ok being integer. coord tho, that needs to be float
   @spec rotate_coordinate_list(coordinate, integer) :: coordinate
-  def rotate_coordinate_list({x, y}, angle_deg) do
+  def rotate_coordinate_list(coords, angle_deg) do
+    coords
+    |> Enum.map(&rotate_coordinate(&1, angle_deg))
+  end 
+
+  @spec rotate_coordinate(coordinate, integer) :: coordinate
+  def rotate_coordinate({x, y}, angle_deg) do
     angle_rad = deg_to_rad(angle_deg)
     x1 = x * :math.cos(angle_rad) - y * :math.sin(angle_rad)
     y1 = y * :math.cos(angle_rad) + x * :math.sin(angle_rad)
@@ -25,4 +33,11 @@ defmodule Chukinas.Dreadnought.Cartesian do
     angle_deg * :math.pi() / 180
   end
 
+  def mirror_x({x, y}) do
+    {x, -y}
+  end
+
+  def mirror_y({x, y}) do
+    {-x, y}
+  end
 end
