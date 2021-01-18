04b2b3901735ef71dcc62ad92be1a8922e995902
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-01-18 08:38:58 -0500

    Add mirror, add, subtract, and center of rotation
    There are all changes to Dreadnought.Point
    
    New mirror function essentially flips the sign of both numbers. This was
    needed for the new subtract function, which was in turn needed for the
    new center of rotation for the rotate function.
    
    Also, added more uses of the `is_point` guard.

--- lib/chukinas/dreadnought/point.ex
+++ lib/chukinas/dreadnought/point.ex
@@ -59,34 +59,46 @@ defmodule Chukinas.Dreadnought.Point do
       iex> alias Chukinas.Dreadnought.{Angle, Point}
       iex> point = {0, 1}
       iex> angle = Angle.new(90)
-      iex> Point.rotate(point, angle) |> Point.change_precision()
+      iex> Point.rotate(point, angle) |> Point.set_precision()
       {-1.0, 0.0}
 
       iex> alias Chukinas.Dreadnought.{Angle, Point}
+      iex> point = {2, 0}
+      iex> rotation_point = {4, 0}
       iex> angle = Angle.new(180)
-      iex> Point.rotate([{1, 2.5}, {0, 0}], angle) |> Point.change_precision(1)
+      iex> Point.rotate(point, angle, rotation_point) |> Point.set_precision(:int)
+      {6, 0}
+
+      iex> alias Chukinas.Dreadnought.{Angle, Point}
+      iex> angle = Angle.new(180)
+      iex> Point.rotate([{1, 2.5}, {0, 0}], angle) |> Point.set_precision(1)
       [{-1.0, -2.5}, {0.0, 0.0}]
 
       iex> alias Chukinas.Dreadnought.{Angle, Point}
       iex> angle = Angle.new(90)
-      iex> Point.rotate(%{a: {1, 2}, b: {0, 0}}, angle) |> Point.change_precision()
+      iex> Point.rotate(%{a: {1, 2}, b: {0, 0}}, angle) |> Point.set_precision()
       %{a: {-2.0, 1.0}, b: {0.0, 0.0}}
   """
-  def rotate(points, %Angle{}=angle) when is_map(points) do
+  def rotate(points, angle, rotation_point \\ {0, 0})
+  def rotate(points, angle, rotation_point) when is_map(points) do
     points
-    |> update_map_values(&rotate(&1, angle))
+    |> update_map_values(&rotate(&1, angle, rotation_point))
   end 
-  
-  def rotate(points, %Angle{}=angle) when is_list(points) do
+  def rotate(points, angle, rotation_point) when is_list(points) do
     points
-    |> Enum.map(&rotate(&1, angle))
+    |> Enum.map(&rotate(&1, angle, rotation_point))
   end 
-
-  def rotate({x, y}=point, %Angle{rad: angle}) when is_point(point) do
+  def rotate({x, y}=point, %Angle{rad: angle}, {0, 0}) when is_point(point) do
     {
       (x * :math.cos(angle)) - (y * :math.sin(angle)),
       (y * :math.cos(angle)) + (x * :math.sin(angle))
     }
+  end 
+  def rotate(point, angle, rotation_point) when is_point(rotation_point) do
+    point
+    |> subtract(rotation_point)
+    |> rotate(angle, {0, 0})
+    |> add(rotation_point)
   end 
 
   # *** *******************************
@@ -118,19 +130,63 @@ defmodule Chukinas.Dreadnought.Point do
     {-x, y}
   end
 
-  # *** *******************************
-  # *** ROUND
+  @doc """
+  Mirror a point about the origin, essentially rotating it 180 deg
 
-  def change_precision(points, precision \\ 0)
-  def change_precision(points, precision) when is_list(points) do
-    points
-    |> Enum.map(&change_precision(&1, precision))
+  ## Examples
+
+      iex> Chukinas.Dreadnought.Point.mirror({2, -3.5})
+      {-2, 3.5}
+  """
+  @spec mirror(t()) :: t()
+  def mirror({x, y}) do
+    {-x, -y}
   end
-  def change_precision(points, precision) when is_map(points) do
-    points
-    |> update_map_values(fn point -> change_precision(point, precision) end)
+
+  # *** *******************************
+  # *** ADD, SUBTRACT
+  
+  @doc """
+  Add two points
+
+  Same in function to the simple form of translate.
+
+  ## Examples
+
+      iex> alias Chukinas.Dreadnought.Point
+      iex> Point.add({1, 2.5}, {2, -0.5})
+      {3, 2.0}
+  """
+  def add(point1, point2) when is_point(point1) and is_point(point2) do
+    translate(point1, point2)
   end
-  def change_precision({x, y}=point, precision) when is_point(point) do
+
+  @doc """
+  Subtract second point from first point
+
+  ## Examples
+
+      iex> alias Chukinas.Dreadnought.Point
+      iex> Point.subtract({1, 2.5}, {2, -0.5})
+      {-1, 3.0}
+  """
+  def subtract(point1, point2) when is_point(point1) and is_point(point2) do
+    translate(point1, mirror(point2))
+  end
+
+  # *** *******************************
+  # *** SET PRECISION
+
+  def set_precision(points, precision \\ 0)
+  def set_precision(points, precision) when is_list(points) do
+    points
+    |> Enum.map(&set_precision(&1, precision))
+  end
+  def set_precision(points, precision) when is_map(points) do
+    points
+    |> update_map_values(fn point -> set_precision(point, precision) end)
+  end
+  def set_precision({x, y}=point, precision) when is_point(point) do
     {
       round_number(x, precision),
       round_number(y, precision),
