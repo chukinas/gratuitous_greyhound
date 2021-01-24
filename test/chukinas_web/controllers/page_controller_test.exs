fb12e145ee9c800be645e4f73eae7508c606dbd9
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-01-24 10:20:14 -0500

    Fix page controller test to test for redirect

--- test/chukinas_web/controllers/page_controller_test.exs
+++ test/chukinas_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ChukinasWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Chukinas"
+    assert html_response(conn, 302) =~ "dreadnought"
   end
 end
