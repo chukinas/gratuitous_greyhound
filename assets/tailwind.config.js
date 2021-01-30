54d111abf322bcb32e06d63247234754bb1a066a
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-01-30 17:54:26 -0500

    Make world map responsive
    Four breakpoints. Now Islands and ship can be seen clearly on any size
    screen.

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    scale: {
+      '25': '.25',
+      '50': '.50',
+      '75': '.75',
+      '100': '1',
     extend: {
       fontFamily: {
         'display': ['"Roboto Slab"', 'serif']
