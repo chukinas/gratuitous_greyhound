2fc982a3b839c115e085f7ae6fa10d560ced56cd
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-01-31 20:15:02 -0500

    Print elapsed time to console on Check Pos click

--- assets/js/dreadnought.js
+++ assets/js/dreadnought.js
@@ -1,11 +1,26 @@
+function buildGetDurationFunction() {
+  const startTime = Date.now();
+  return () => {
+    const currentTime = Date.now();
+    const elapsedMilliseconds = currentTime - startTime;
+    return (elapsedMilliseconds / 1000);
+  }
+}
+
+function getSliderPosition() {
+  const slider = document.getElementById("slider");
+  const xPosition = slider.getBoundingClientRect().left
+  return Math.round(xPosition)
+}
+
 let DreadnoughtHooks = {}
 
 DreadnoughtHooks.PositionCheck = {
   mounted() {
+    const getDuration = buildGetDurationFunction();
     this.el.addEventListener("click", () => {
-      const slider = document.getElementById("slider");
-      const xPosition = slider.getBoundingClientRect().left
-      this.pushEvent("check_time", {rand: Math.round(xPosition)})
+      console.log(getDuration());
+      this.pushEvent("check_time", {rand: getSliderPosition()})
     })
   }
 }
