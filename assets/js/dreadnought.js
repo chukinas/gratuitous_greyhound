edeec71e6973a7190d53b0d80453c195b547f25a
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-02-01 21:16:00 -0500

    Output ideal and actual slider position to console
    This happens with each click of the "Check Position" button. In
    retrospect, I didn't need to use a LiveView push event at all. But at
    least it's now there for reference.
    
    Observation #1: It's interesting that the actual position leads the ideal by about 10px.
    Maybe this is because the elapsed-time closure initiates after the
    animation has already begun? Either way, I think once the gsap code is
    moved into the hooks and especially after starting the game on a synced
    timer, then that difference will become negligable.
    
    Observation #2: It's confirmed: leaving the tab will pause the
    animation. I knew this would happen, but it's nice to have it
    quantified. The ideal and actual positions vary wildly after that.
    
    Observation #3: The zoom level also affects the actual position. I'll
    have to take this into account. At smaller screen sizes, everything
    shrinks, so the actual is smaller than the ideal.
    
    Observation #4: Mounting new DOM elements via liveview updates does not
    affect the position readings. Everything continues to animate smoothly.

--- assets/js/dreadnought.js
+++ assets/js/dreadnought.js
@@ -1,26 +1,43 @@
-function buildGetDurationFunction() {
+const DISTANCE = 1000; // pixels
+const DURATION = 10; // seconds
+const SPEED = DISTANCE / DURATION; // px/sec
+
+function initClosure_getElapsedSeconds() {
   const startTime = Date.now();
   return () => {
     const currentTime = Date.now();
     const elapsedMilliseconds = currentTime - startTime;
-    return (elapsedMilliseconds / 1000);
+    return (elapsedMilliseconds / DISTANCE);
   }
 }
 
 function getSliderPosition() {
   const slider = document.getElementById("slider");
-  const xPosition = slider.getBoundingClientRect().left
-  return Math.round(xPosition)
+  return slider.getBoundingClientRect().left;
+}
+
+function calcCurrentTripDuration(elapsedSecondsTotal) {
+  return elapsedSecondsTotal % DURATION;
+}
+
+function calcIdealPosition(elapsedSecondsCurrentTrip) {
+  return SPEED * elapsedSecondsCurrentTrip;
 }
 
 let DreadnoughtHooks = {}
 
 DreadnoughtHooks.PositionCheck = {
   mounted() {
-    const getDuration = buildGetDurationFunction();
+    const getElapsedSeconds = initClosure_getElapsedSeconds();
     this.el.addEventListener("click", () => {
-      console.log(getDuration());
-      this.pushEvent("check_time", {rand: getSliderPosition()})
+      const actualPosition = getSliderPosition();
+      const elapsedSecondsTotal = getElapsedSeconds();
+      const elapsedSecondsCurrentTrip = calcCurrentTripDuration(elapsedSecondsTotal);
+      const idealPosition = calcIdealPosition(elapsedSecondsCurrentTrip);
+      const positionDelta = actualPosition - idealPosition;
+      const vals = {elapsedSecondsTotal, elapsedSecondsCurrentTrip, idealPosition, actualPosition, positionDelta}
+      console.table(vals)
+      this.pushEvent("check_time", {rand: actualPosition})
     })
   }
 }
