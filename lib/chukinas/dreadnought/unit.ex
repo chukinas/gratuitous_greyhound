137fb57fd60b79aeda0732dc96bbcf4d2a19ee6d
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-02-05 18:50:57 -0500

    Add demo unit to assigns

--- lib/chukinas/dreadnought/unit.ex
+++ lib/chukinas/dreadnought/unit.ex
@@ -1,3 +1,39 @@
-defmodule Chukinas.Dreadnought.Model.Unit do
+defmodule Chukinas.Dreadnought.Unit do
+  @moduledoc """
+  Represents a ship or some other combat unit
+  """
+
+  # *** *******************************
+  # *** TYPES
+
+  use TypedStruct
+
+  typedstruct enforce: true do
+    # ID must be unique within the world
+    field :id, number()
+
+    # Hull and Turrets describe the physical properties of the unit.
+    # field :hull, Hull.t()
+    # field :turrets, [Turret.t()]
+
+    # The deck is self-contained and has containers for cards in various states,
+    # for example in-hand, discarded, and destroyed
+    # field :deck, Deck.t()
+
+    # Commands draw their data from command cards from the deck.
+    # The cards' data is copied to here. Keeps a nice separation of concerns.
+    # field :commands, [Command.t()]
+  end
+
+  # *** *******************************
+  # *** NEW
+
+  def new() do
+    %__MODULE__{
+      id: 2,
+      # hull: Hull.new(),
+      # turrets: Turret.new(),
+    }
+  end
 
 end
--- lib/chukinas_web/live/dreadnought_live.ex
+++ lib/chukinas_web/live/dreadnought_live.ex
@@ -1,13 +1,13 @@
 defmodule ChukinasWeb.DreadnoughtLive do
   use ChukinasWeb, :live_view
-  alias Chukinas.Dreadnought.Deck
+  alias Chukinas.Dreadnought.Unit
   alias ChukinasWeb.DreadnoughtView
 
   @impl true
   def mount(_params, _session, socket) do
     socket = socket
     |> assign(page_title: "Dreadnought")
-    |> assign(deck: Deck.new(1) |> Deck.draw(1))
+    |> assign(units: [Unit.new()])
     |> assign(time_checks: [])
     {:ok, socket}
   end
