ab42af6e498f57a462bb251e8c54e94e275dd55d
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-02-11 21:22:30 -0500

    Add translation to Position protocol

--- lib/chukinas/geometry/position.ex
+++ lib/chukinas/geometry/position.ex
@@ -3,14 +3,27 @@ alias Chukinas.Geometry.{Position, Pose, Point}
 defprotocol Position do
 
   @type position_tuple() :: {number(), number()}
+  @type translation() :: position_tuple() | t()
 
   @spec to_tuple(t()) :: position_tuple()
   def to_tuple(position)
+
+  @spec translate(t(), translation()) :: t()
+  def translate(positionable, translation)
 
 end
 
 defimpl Position, for: [Pose, Point] do
 
   def to_tuple(%{x: x, y: y}), do: {x, y}
+
+  def translate(positionable, translation) do
+    Map.merge(positionable, sanitize_translation(translation), fn _k, orig, trans ->
+      orig + trans
+    end)
+  end
+
+  defp sanitize_translation({x, y}), do: %{x: x, y: y}
+  defp sanitize_translation(%{x: x, y: y}), do: %{x: x, y: y}
 
 end
--- test/chukinas/geometry/position_test.exs
+++ test/chukinas/geometry/position_test.exs
@@ -1,12 +1,16 @@
+alias Chukinas.Geometry.{PositionTest, Position, Pose, Point}
+
 ExUnit.start()
 
-defmodule Chukinas.Geometry.PositionTest do
-  alias Chukinas.Geometry.{Position, Pose, Point}
+defmodule PositionTest do
   use ExUnit.Case, async: true
 
-  # TODO do this for point as well
   test "to tuple" do
     assert {0, 0} = Point.new(0, 0) |> Position.to_tuple()
     assert {0, 0} = Pose.new(0, 0, 0) |> Position.to_tuple()
+  end
+
+  test "translate" do
+    assert {1, 1} = Point.new(0, 0) |> Position.translate({1, 1}) |> Position.to_tuple()
   end
 end
