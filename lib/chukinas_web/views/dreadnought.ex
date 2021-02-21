395ae745626b81c5f9d25319277f25e6dd3857db
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-02-20 20:14:06 -0500

    Scaffold `message` shared compoment
    This proof of concept shows how to create a template that renders an
    @inner_content
    
    Based on this article:
    https://fullstackphoenix.com/tutorials/nested-templates-and-layouts-in-phoenix-framework

--- lib/chukinas_web/views/dreadnought.ex
+++ lib/chukinas_web/views/dreadnought.ex
@@ -1,5 +1,17 @@
 defmodule ChukinasWeb.DreadnoughtView do
   use ChukinasWeb, :view
+
+  def message(assigns \\ %{}, do: block) do
+    render_template("_message.html", assigns, block)
+  end
+
+  defp render_template(template, assigns, block) do
+    assigns =
+      assigns
+      |> Map.new()
+      |> Map.put(:inner_content, block)
+    render template, assigns
+  end
 end
 
 # https://bernheisel.com/blog/phoenix-liveview-and-views
