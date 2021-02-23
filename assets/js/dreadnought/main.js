d2693ff263d6eeeee3c09fb86fa282d4b393c025
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-02-22 19:23:04 -0500

    Restructure assets/js dir
    Broke it out into hooks and core dirs. I've been meaning to do this for
    a while.

--- assets/js/dreadnought/main.js
+++ assets/js/dreadnought/main.js
@@ -1,13 +1,11 @@
-import CommandCardHooks from "./command_card.js";
-import SliderHooks from "./constant_game_time.js";
-import UnitHooks from "./unit.js";
-import { CheckGsapLoad } from "./gsap_experiment.js";
-import SegmentHooks from "./segment.js";
+import CommandCardHooks from "./hooks/command_card.js";
+// import SliderHooks from "./constant_game_time.js";
+import UnitHooks from "./hooks/unit.js";
+// import { CheckGsapLoad } from "./gsap_experiment.js";
+import SegmentHooks from "./hooks/segment.js";
 
 export default {
   ...CommandCardHooks,
-  ...SliderHooks, 
   ...UnitHooks, 
   ...SegmentHooks,
-  CheckGsapLoad,
 }
