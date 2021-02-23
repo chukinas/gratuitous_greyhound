8fca5449456ff59d34fde68501bb20f56d37d6bd
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-02-22 20:21:10 -0500

    Demo how one tween interupts another

--- assets/js/dreadnought/main.js
+++ assets/js/dreadnought/main.js
@@ -1,11 +1,12 @@
 import CommandCardHooks from "./hooks/command_card.js";
 // import SliderHooks from "./constant_game_time.js";
 import UnitHooks from "./hooks/unit.js";
-// import { CheckGsapLoad } from "./gsap_experiment.js";
+import GsapHooks from "./proofs_of_concept/gsap_experiment.js";
 import SegmentHooks from "./hooks/segment.js";
 
 export default {
   ...CommandCardHooks,
   ...UnitHooks, 
+  ...GsapHooks,
   ...SegmentHooks,
 }
