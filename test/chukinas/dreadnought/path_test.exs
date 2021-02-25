fa45d5ef2db3b963c57ce71f454f2bfcd6b63b8c
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-02-24 20:58:39 -0500

    Write some failing tests for Paths
    I didn't yet have any low level tests for Paths. I had some tests for
    the CommandQueue.

---
+++ test/chukinas/dreadnought/path_test.exs
@@ -0,0 +1,29 @@
+ExUnit.start()
+
+defmodule PathTest do
+  use ExUnit.Case, async: true
+  use DreadnoughtHelpers
+
+  test "calculate end pose of straight path" do
+    actual_end_pose =
+      Path.new(
+        pose: Pose.origin(),
+        length: :math.pi()
+      )
+      |> Path.get_end_pose()
+    expected_end_pose = Pose.new(1, 0, 0)
+    assert match_numerical_map? expected_end_pose, actual_end_pose
+  end
+
+  test "calculate end pose of turn path" do
+    actual_end_pose =
+      Path.new(
+        pose: Pose.new(-1, 0, 90),
+        length: :math.pi(),
+        angle: 90
+      )
+      |> Path.get_end_pose()
+    expected_end_pose = Pose.new(0, 1, 0)
+    assert match_numerical_map? expected_end_pose, actual_end_pose
+  end
+end
