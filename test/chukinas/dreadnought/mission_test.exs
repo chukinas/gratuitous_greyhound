149c46875ab4bcd080100a77c548b28fba29958e
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-02-26 21:22:52 -0500

    Write failing test for Mission.issue_command/3

---
+++ test/chukinas/dreadnought/mission_test.exs
@@ -0,0 +1,35 @@
+ExUnit.start()
+
+defmodule MissionTest do
+  use ExUnit.Case, async: true
+  use DreadnoughtHelpers
+
+  test "Issue Command" do
+    mission =
+      Mission.new()
+      |> Mission.issue_command(unit: 1, card: 1, segment: 1)
+    actual_end_pose =
+      mission
+      |> Mission.unit(1)
+      |> Unit.segment(1)
+      |> Segment.end_pose()
+    expected_end_pose = Pose.new(100, 100, 0)
+    assert match_numerical_map? expected_end_pose, actual_end_pose
+
+    actual_hand_ids =
+      mission
+      |> Mission.deck(1)
+      |> Deck.hand()
+      |> Enum.map(&(&1.id))
+    expected_hand_ids = [2, 3, 4, 5, 6]
+    assert match_numerical_map? expected_hand_ids, actual_hand_ids
+
+    actual_discard_ids =
+      mission
+      |> Mission.deck(1)
+      |> Deck.discards()
+      |> Enum.map(&(&1.id))
+    expected_discard_ids = [1]
+    assert match_numerical_map? expected_discard_ids, actual_discard_ids
+  end
+end
