3237f5997bd499aeaf4a836d22ea0752b1f1897a
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-03-06 11:34:54 -0500

    Rename worldTimeline to timelines.js

--- assets/js/dreadnought/hooks/unit.js
+++ assets/js/dreadnought/hooks/unit.js
@@ -1,4 +1,4 @@
-import * as worldTimeline from "../core/worldTimeline.js";
+import * as worldTimeline from "../core/timelines.js";
 
 // --------------------------------------------------------
 // CONSTANTS
