fd6e8884f1c4f752efe6f0b1cbbf007fb1a17aef
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-03-09 16:57:16 -0500

    Move the rendering of commands to command panel
    I've lost functionality, but I'll hook that up later.

--- lib/chukinas_web/live/dreadnought/command_panel_live.ex
+++ lib/chukinas_web/live/dreadnought/command_panel_live.ex
@@ -1,10 +1,36 @@
 defmodule ChukinasWeb.Dreadnought.CommandPanel do
   use Phoenix.LiveComponent
 
+  # TODO delete templates/dreadnought/command.html.leex
+
   @impl true
   def render(assigns) do
+    assigns |> IOP.inspect("command panel assigns!")
     ~L"""
-    <p>Hi!</p>
+    <div class="bg-red-500 opacity-50 fixed inset-x-0 bottom-0">
+      <div class="flex gap-4">
+        <div class="flex-1"></div>
+        <button class="p-2 text-white text-xl font-bold bg-black bg-opacity-20"><<</button>
+        <button
+          id="issueCommand"
+          phx-hook="IssueCommand"
+          class="p-2 text-white text-xl font-bold bg-black bg-opacity-20 disabled:opacity-25"
+        >
+          Issue Command
+        </button>
+        <button class="p-2 text-white text-xl font-bold bg-black bg-opacity-20">>></button>
+        <div class="flex-1"></div>
+      </div>
+      <div class="flex">
+        <div class="flex-1"></div>
+        <div class="flex-1 order-3"></div>
+        <div class="grid gap-4 p-4 grid-cols-4 justify-center ">
+          <%= for command <- @commands do %>
+            <%= ChukinasWeb.DreadnoughtView.command %{socket: @socket, command: command} %>
+          <% end %>
+        </div>
+      </div>
+    </div>
     """
 #     ~L"""
 #     <h1 class="m-4 text-lg">LiveComponent</h1>
