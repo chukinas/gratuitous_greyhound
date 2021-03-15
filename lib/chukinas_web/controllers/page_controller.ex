8787bd42deca882468d88c4bd67d749109d7f647
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-03-15 13:37:07 -0400

    Replace `/` redirect with empty page

--- lib/chukinas_web/controllers/page_controller.ex
+++ lib/chukinas_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule ChukinasWeb.PageController do
   use ChukinasWeb, :controller
 
   def index(conn, _params) do
-    redirect(conn, to: "/dreadnought")
+    render conn, "index.html"
   end
 end
