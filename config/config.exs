52ade93c5ff87b778acea43ffbf921d98aaee3eb
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-03-30 19:30:09 -0400

    rename the sendgrid api key env var

--- config/config.exs
+++ config/config.exs
@@ -26,7 +26,7 @@ config :phoenix, :json_library, Jason
 # Email configuration
 config :chukinas, Chukinas.Mailer,
   adapter: Bamboo.SendGridAdapter,
-  api_key: {:system, "SMTP_PASSWORD"}
+  api_key: {:system, "SENDGRID_API_KEY"}
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
