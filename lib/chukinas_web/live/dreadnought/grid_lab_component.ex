a18bd57df3f4ab31294c12f1d3edc3910e322ce6
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-03-31 18:21:49 -0400

    Add arena div to grid lab

--- lib/chukinas_web/live/dreadnought/grid_lab_component.ex
+++ lib/chukinas_web/live/dreadnought/grid_lab_component.ex
@@ -4,13 +4,25 @@ defmodule ChukinasWeb.Dreadnought.GridLabComponent do
   @impl true
   def render(assigns) do
     ~L"""
-    <p
+    <div
       id="world"
-      class="bg-blue-400"
+      class="bg-blue-400 relative"
       style="width:<%= @world.width %>px; height: <%= @world.height %>px"
     >
       WORLD
-    </p>
+      <div
+        id="arena"
+        class="bg-green-300 absolute"
+        style="
+          left: 500px;
+          top: 500px;
+          width:500px;
+          height: 500px
+        "
+      >
+      ARENA
+      </div>
+    </div>
     """
   end
 
