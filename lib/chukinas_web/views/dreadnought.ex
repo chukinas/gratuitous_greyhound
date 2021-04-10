d2fbd28f976bf2da13093fa977169279e8caf6d3
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-04-10 17:23:09 -0400

    Orient ship

--- lib/chukinas_web/views/dreadnought.ex
+++ lib/chukinas_web/views/dreadnought.ex
@@ -38,6 +38,9 @@ defmodule ChukinasWeb.DreadnoughtView do
       image_size_x: 122,
       image_size_y: 57,
       svg_path: "M 73,56 49,56 22,57 2,52 0,38 19,28 l 52,2 20,6 9,7 0,5 z",
+      # These are used merely for getting more useful calculated values?
+      # TODO, also, many of these are just good guesses.
+      # When I auto-generate this with macros, they'll be exact.
       center_x: 68,
       center_y: 44,
       min_x: 0,
@@ -50,23 +53,23 @@ defmodule ChukinasWeb.DreadnoughtView do
 
   def add_centered_bounding_rect(item) when is_map(item) do
     # Returns a box whose center shares the item's center
-    size_x = max(
+    half_x_size = max(
       abs(item.center_x - item.min_x),
       abs(item.center_x - item.max_x)
     )
     |> ceil
-    |> double
-    size_y = max(
+    half_y_size = max(
       abs(item.center_y - item.min_y),
       abs(item.center_y - item.max_y)
     )
     |> ceil
-    |> double
     rect = %{
-      x: round(item.center_x - size_x / 2),
-      y: round(item.center_y - size_y / 2),
-      width: size_x,
-      height: size_y
+      x: round(item.center_x - half_x_size),
+      y: round(item.center_y - half_y_size),
+      half_width: half_x_size,
+      half_height: half_y_size,
+      width: half_x_size |> double,
+      height: half_y_size |> double,
     }
     Map.put item, :rect, rect
   end
