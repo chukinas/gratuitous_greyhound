0f9abfcd2dfe71bbeeb2603a16c8786dfd252dd6
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-04-10 19:48:44 -0400

    Remove command_card.js

--- assets/js/dreadnought/main.js
+++ assets/js/dreadnought/main.js
@@ -1,12 +1,9 @@
-import CommandCardHooks from "./hooks/command_card.js";
-// import SliderHooks from "./constant_game_time.js";
 import UnitHooks from "./hooks/unit.js";
 import GsapHooks from "./proofs_of_concept/gsap_experiment.js";
 import ZoomHooks from "./hooks/pinch_zoom.js";
 import PanHooks from "./hooks/pan.js";
 
 export default {
-  ...CommandCardHooks,
   ...UnitHooks, 
   ...GsapHooks,
   ...ZoomHooks,
