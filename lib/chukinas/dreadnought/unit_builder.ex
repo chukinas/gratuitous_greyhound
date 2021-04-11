5c07eb0ad55876d6073139f1417fad1a650ccaf7
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-04-11 08:30:43 -0400

    Add first macro to dynamically generate functions

--- lib/chukinas/dreadnought/unit_builder.ex
+++ lib/chukinas/dreadnought/unit_builder.ex
@@ -2,6 +2,13 @@ alias Chukinas.Dreadnought.Unit.Builder
 
 defmodule Builder do
 
+  file_path = "assets/static/images/test.svg"
+  {:ok, svg_content} = File.read file_path
+  svg_map = XmlToMap.naive_map(svg_content)
+  for key <- Map.keys(svg_map["svg"]["#content"]["g"]["#content"]["path"]) do
+    def print_this(unquote(key)), do: IOP.inspect(unquote(key))
+  end
+
   def form("red_ship_2") do
     mymap = %{
       mounts: [
