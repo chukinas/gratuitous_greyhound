56883347416a0257ba7f5f54f31c7d1fcb865b3d
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-04-28 07:29:28 -0400

    Extract dyn world comp markup

--- lib/chukinas_web/live/dreadnought/dynamic_world_component.ex
+++ lib/chukinas_web/live/dreadnought/dynamic_world_component.ex
@@ -6,91 +6,9 @@ defmodule ChukinasWeb.Dreadnought.DynamicWorldComponent do
 
   @impl true
   def render(assigns) do
-    #inspect_assigns assigns, "render assigns"
     # TODO is it a problem that there isn't a single root element here? %>
     # TODO might be worth having a containing div so I only have to set margin and size once
-    ~L"""
-    <div id="turn-number" data-turn-number="<%= @turn_number %>" phx-hook="TurnNumber"></div>
-    <div
-      id="arena"
-      class="absolute rounded-3xl"
-      style="
-        box-shadow: inset 10px 10px 50px, 2px 2px 10px aquamarine;
-        left: <%= @margin.width %>px;
-        top: <%= @margin.height %>px;
-        width:<%= @grid.width %>px;
-        height: <%= @grid.height %>px
-      "
-    >
-      <div
-        id="arenaGrid"
-        style="
-          display: grid;
-          grid-auto-columns: <%= @grid.square_size %>px;
-          grid-auto-rows: <%= @grid.square_size %>px;
-        "
-      >
-        <%= for unit <- @units do %>
-        <%= if unit.id in @player_actions.active_unit_ids do %>
-        <%= for square <- unit.cmd_squares do %>
-        <button
-          id="gridSquareTarget-<%= square.id %>"
-          class="p-0.5 hover:p-0 pointer-events-auto"
-          style="
-            grid-column-start: <%= square.column %>;
-            grid-row-start: <%= square.row %>;
-          "
-          phx-click="select_square"
-          phx-target="<%= @myself %>"
-          phx-value-x="<%= square.center.x %>"
-          phx-value-y="<%= square.center.y %>"
-          phx-value-unit_id="<%= unit.id %>"
-          phx-value-type="<%= square.path_type %>"
-        >
-          <div
-            id="gridSquareVisible-<%= square.id %>"
-            class="
-              <%= case square.path_type do
-                :sharp_turn -> "bg-red-400"
-                :straight -> "bg-green-400"
-                _ -> "bg-yellow-400"
-              end %>
-              h-full rounded-sm bg-opacity-20 pointer-events-none
-            "
-          >
-          </div>
-        </button>
-        <% end %>
-        <% end %>
-        <% end %>
-      </div>
-    </div>
-    <svg
-      id="svg_paths"
-      class="absolute opacity-20"
-      viewBox="0 0 <%= @grid.width %> <%= @grid.height %> "
-      style="
-        left: <%= @margin.width %>px;
-        top: <%= @margin.height %>px;
-        width:<%= @grid.width %>px;
-        height: <%= @grid.height %>px
-      "
-    >
-      <%= for unit <- @units do %>
-      <path
-        id="unit-<%= unit.id %>-lastPath"
-        d="<%= unit.maneuver_svg_string %>"
-        style="stroke-linejoin:round;stroke-width:20;stroke:#fff;fill:none"
-      />
-      <% end %>
-    </svg>
-    <%= for unit <- @units do %>
-    <%= ChukinasWeb.DreadnoughtView.render "unit3.html",
-      socket: @socket,
-      unit: unit,
-      margin: @margin %>
-    <% end %>
-    """
+    ChukinasWeb.DreadnoughtView.render("component_dynamic_world.html", assigns)
   end
 
   @impl true
