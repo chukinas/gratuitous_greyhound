4548b7597c2daf93f773e63ef34eb7ce627da4b6
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-05-22 06:11:06 -0400

    Add error_icon_text_field to error_helpers

--- lib/chukinas_web/live/dreadnought/join_room_component.ex
+++ lib/chukinas_web/live/dreadnought/join_room_component.ex
@@ -58,12 +58,7 @@ defmodule JoinRoomComponent do
           <%= label f, field, label_class() %>
           <div class="mt-1 relative rounded-md shadow-sm">
             <%= text_input f, field, text_input_class() %>
-            <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
-              <!-- Heroicon name: solid/exclamation-circle -->
-              <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
-              </svg>
-            </div>
+            <%= error_icon_text_field f, field %>
           </div>
           <%= error_paragraph f, field, error_paragraph_class() %>
         </div>
@@ -78,8 +73,5 @@ defmodule JoinRoomComponent do
   defp label_class, do: [class: "block text-sm font-medium text-gray-700"]
   defp error_paragraph_class do
     "mt-2 text-sm text-red-600"
-  end
-  defp error_sensitive_text_input(form, field) do
-    nil
   end
 end
--- lib/chukinas_web/views/error_helpers.ex
+++ lib/chukinas_web/views/error_helpers.ex
@@ -18,6 +18,7 @@ defmodule ChukinasWeb.ErrorHelpers do
   end
 
   def error_paragraph(form, field, class) do
+    IOP.inspect form, "error p"
     Enum.map(Keyword.get_values(form.errors, field), fn error ->
       content_tag(:p, translate_error(error),
         class: "invalid-feedback " <> class,
@@ -25,6 +26,21 @@ defmodule ChukinasWeb.ErrorHelpers do
       )
     end)
   end
+
+  def error_icon_text_field(form, field) do
+    if not (form.errors |> Keyword.get_values(field) |> Enum.empty?), do: __error_icon__()
+  end
+
+  defp __error_icon__ do
+    ~E"""
+    <div class="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+      <svg class="h-5 w-5 text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+        <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
+      </svg>
+    </div>
+    """
+  end
+
   @doc """
   Translates an error message using gettext.
   """
