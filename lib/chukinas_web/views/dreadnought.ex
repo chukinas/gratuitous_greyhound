ce7a0218ed48c6dafd9429405cff517aee96887e
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-07-03 21:15:47 -0400

    Move maneuver_path to DreadnoughtPlayView

--- lib/chukinas_web/views/dreadnought.ex
+++ lib/chukinas_web/views/dreadnought.ex
@@ -10,17 +10,6 @@ defmodule ChukinasWeb.DreadnoughtView do
   alias Chukinas.Util.Opts
 
 
-  def maneuver_path(%Ev.Maneuver{} = path, unit_id) do
-    assigns =
-      path
-      |> Map.from_struct
-      |> Map.put(:unit_el_id, "unit-#{unit_id}")
-    render("maneuver_path.html", assigns)
-  end
-  def maneuver_path(_, _) do
-    nil
-  end
-
   def unit_event(%Ev.Maneuver{} = event) do
     standard_attributes(event, "maneuver") ++ [
       data_attr("id", event.id),
--- lib/chukinas_web/views/dreadnought_play_view.ex
+++ lib/chukinas_web/views/dreadnought_play_view.ex
@@ -1,3 +1,18 @@
 defmodule ChukinasWeb.DreadnoughtPlayView do
+
   use ChukinasWeb, :view
+  alias Chukinas.Dreadnought.Unit.Event, as: Ev
+
+  def maneuver_path(%Ev.Maneuver{} = path, unit_id) do
+    assigns =
+      path
+      |> Map.from_struct
+      |> Map.put(:unit_el_id, "unit-#{unit_id}")
+    render("maneuver_path.html", assigns)
+  end
+
+  def maneuver_path(_, _) do
+    nil
+  end
+
 end
