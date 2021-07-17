5aef67ed6eae6fc1b67a1fcf762476fb1a80186e
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-07-17 11:26:04 -0400

    Extract dropshadow markup; yet unused

--- lib/chukinas_web/views/sprite.ex
+++ lib/chukinas_web/views/sprite.ex
@@ -4,20 +4,26 @@ defmodule ChukinasWeb.SpriteView do
   use ChukinasWeb.Components
   use Chukinas.PositionOrientationSize
   use Chukinas.LinearAlgebra
-  alias Chukinas.Geometry.Rect
+  alias Chukinas.Dreadnought.Sprite
 
-  def static_sprite(opts \\ []) do
-    # TODO I don't like this use of 'opts'. The two 'opts' are anything but. They're required.
-    sprite = Keyword.fetch!(opts, :sprite)
-    rect = Rect.from_rect(sprite)
+  def static_sprite(conn, %Sprite{} = sprite) do
     assigns = [
-      socket: Keyword.fetch!(opts, :socket),
-      rect: rect,
+      socket: conn,
+      sprite: sprite,
       image_file_path: sprite.image_file_path,
       image_size: sprite.image_size,
       image_clip_path: sprite.image_clip_path,
       transform: sprite.image_origin |> position_add(sprite) |> position_multiply(-1),
-      sprite_viewbox: ChukinasWeb.Shared.viewbox(sprite),
+    ]
+    render("static_sprite.html", assigns)
+  end
+
+  def drop_shadow(%Sprite{} = sprite) do
+    assigns = [
+      sprite: sprite,
+      image_size: sprite.image_size,
+      image_clip_path: sprite.image_clip_path,
+      transform: sprite.image_origin |> position_add(sprite) |> position_multiply(-1),
       image_viewbox: ChukinasWeb.Shared.viewbox(sprite.image_size)
     ]
     render("static_sprite.html", assigns)
