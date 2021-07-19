27f20f7fec0ff625f5b5a0520367b987fb68c817
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-07-19 07:29:19 -0400

    Render only muzzle flash animations

--- lib/chukinas/dreadnought/animations.ex
+++ lib/chukinas/dreadnought/animations.ex
@@ -10,14 +10,14 @@ defmodule Chukinas.Dreadnought.Animations do
 
   def simple_muzzle_flash(pose, delay \\ 0) when is_number(delay) do
     "simple muzzle flash"
-    |> Animation.new(pose, delay)
+    |> Animation.new(:muzzle_flash, pose, delay)
     |> Animation.put(rand_explosion_frame(0))
     |> Animation.fade(0.5)
   end
 
   def large_muzzle_flash(pose, delay \\ 0) when is_number(delay) do
     "large muzzle flash"
-    |> Animation.new(pose, delay)
+    |> Animation.new(:muzzle_flash, pose, delay)
     |> Animation.put(rand_explosion_frame(0.05))
     #|> Animation.put_frame(:blue, "muzzle_flash_A", 0.1)
     |> Animation.put_frame(:blue, "muzzle_flash_A", 0.1)
@@ -28,7 +28,7 @@ defmodule Chukinas.Dreadnought.Animations do
 
   def ordnance_hit(pose, delay \\ 0) when is_number(delay) do
     "ordnance_hit"
-    |> Animation.new(pose, delay)
+    |> Animation.new(:hit, pose, delay)
     |> Animation.put(rand_explosion_frame(0.05))
     |> Animation.put_frame(:red, "muzzle_flash", 0)
     |> Animation.fade(0.5)
@@ -38,6 +38,13 @@ defmodule Chukinas.Dreadnought.Animations do
   # *** REDUCERS
 
   defdelegate repeat(animation), to: Animation
+
+  # *** *******************************
+  # *** LIST REDUCERS
+
+  def list_muzzle_flashes(animations) when is_list(animations) do
+    Enum.filter animations, &Animation.muzzle_flash?/1
+  end
 
   # *** *******************************
   # *** CONVERTERS
