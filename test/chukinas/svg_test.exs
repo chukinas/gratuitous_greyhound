d9a5f60479699e8c9ff5a3a45984fd53cdeb5e01
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-07-22 16:22:40 -0400

    Fix broken SVG tests

--- test/chukinas/svg_test.exs
+++ test/chukinas/svg_test.exs
@@ -5,6 +5,7 @@ ExUnit.start()
 defmodule Chukinas.SvgTest do
   use ExUnit.Case, async: true
   use Chukinas.PositionOrientationSize
+  alias Chukinas.Svg
 
   test "svg path string from straight path" do
     assert "M 0 0 L 10 0" = Paths.new_straight(0, 0, 0, 10) |> Svg.get_path_string()
