bbe2e6378224febabdc69e5b3ea13e5c96e60d1a
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-07-25 16:38:43 -0400

    Prove out tiled paper concept

--- lib/chukinas_web/views/layout_view.ex
+++ lib/chukinas_web/views/layout_view.ex
@@ -1,4 +1,9 @@
 defmodule ChukinasWeb.LayoutView do
   use ChukinasWeb, :view
   use ChukinasWeb.Components
+
+  def crinkled_paper_path(socket) do
+    Routes.static_path socket, "/images/crinkled_paper_20210517.jpg"
+  end
+
 end
