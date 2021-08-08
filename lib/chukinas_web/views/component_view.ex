63d5e0d21aaaaa48de5ea7477a3647177a73e8c8
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-08-08 12:54:45 -0400

    add render_header component

--- lib/chukinas_web/views/component_view.ex
+++ lib/chukinas_web/views/component_view.ex
@@ -23,6 +23,11 @@ defmodule ChukinasWeb.ComponentView do
     render("toggle.html", assigns)
   end
 
+  # *** *******************************
+  # *** DOTS
+  #     These are currently only used in the Gallery.
+  #     They show the mounts and mounting point
+
   def render_rotation_center_dot do
     render_dot(position_origin(), "pink")
   end
@@ -36,6 +41,18 @@ defmodule ChukinasWeb.ComponentView do
       rect: Rect.from_centered_square(position, 20),
       color: color
     )
+  end
+
+  # *** *******************************
+  # *** TEXT
+
+  def render_header(value, class) do
+    attrs = []
+    class = """
+    text-4xl sm:text-5xl uppercase font-extrabold tracking-widest text-yellow-400
+    #{class}
+    """
+    Phoenix.HTML.Tag.content_tag :h1, value, attrs ++ [class: class]
   end
 
   def render_label(form, field, display \\ nil) do
@@ -60,27 +77,26 @@ defmodule ChukinasWeb.ComponentView do
   # *** *******************************
   # *** BUTTONS
 
-  # TODO rename `render_button`
-  def render_content_button(content, attrs \\ []) do
-    Phoenix.HTML.Tag.content_tag :button, content, attrs ++ [class: __button_classes__()]
+  def render_button(value, attrs \\ []) do
+    Phoenix.HTML.Tag.content_tag :button, value, attrs ++ [class: __button_classes__()]
   end
 
-  def render_submit(form, value, attrs \\ []) do
+  def render_submit(form, value) do
     opts =
-      attrs
-      |> Keyword.put_new(:class, __button_classes__())
-      |> Keyword.put_new(:disabled, !valid?(form))
+      [
+        class: __button_classes__(),
+        disabled: !valid?(form)
+      ]
     Phoenix.HTML.Form.submit(value, opts)
   end
 
   defp __button_classes__ do
     """
-    flex justify-center
     rounded-md
     border-2 border-yellow-200
     shadow-sm
     w-full px-6 py-3
-    text-lg
+    text-3xl
     text-yellow-200 hover:text-yellow-900 disabled:text-yellow-200
     font-medium hover:font-bold disabled:font-medium
     hover:bg-yellow-100/50 disabled:bg-transparent focus:outline-none
