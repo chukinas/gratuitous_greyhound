2852079180bf866c5187a7d03f73c742f094b907
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-08-10 07:18:36 -0400

    Replace Room with Mission in RoomBackup

--- lib/chukinas/sessions/rooms/room_backup.ex
+++ lib/chukinas/sessions/rooms/room_backup.ex
@@ -1,18 +1,19 @@
+# TODO rename MissionBackup?
 defmodule Chukinas.Sessions.RoomBackup do
 
   use Agent
-  alias Chukinas.Sessions.Room
+  alias Chukinas.Dreadnought.Mission
 
   def start_link(_init_arg) do
     Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
-  @spec put(Room.t) :: :ok
-  def put(%Room{name: room_name} = room) do
-    Agent.update(__MODULE__, &Map.put(&1, room_name, room))
+  @spec put(Mission.t) :: :ok
+  def put(%Mission{} = mission) do
+    Agent.update(__MODULE__, &Map.put(&1, Mission.name(mission), mission))
   end
 
-  @spec pop(String.t) :: Room.t | nil
+  @spec pop(String.t) :: Mission.t | nil
   def pop(room_name) do
     Agent.get_and_update(__MODULE__, &Map.pop(&1, room_name))
   end
