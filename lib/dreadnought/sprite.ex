1275bf8d0225c5ae51dd2717ff51fa3eb439697a
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-08-21 14:33:38 -0400

    Add Sprite.Builder.build func

--- lib/dreadnought/sprite.ex
+++ lib/dreadnought/sprite.ex
@@ -2,33 +2,13 @@ defmodule Dreadnought.Sprite do
 
     use Dreadnought.PositionOrientationSize
     use Dreadnought.TypedStruct
-  alias Dreadnought.Core.Mount
-  alias Dreadnought.Sprite.Image
   alias Dreadnought.Sprite.Spec
-  alias Dreadnought.Util.IdList
 
   # *** *******************************
   # *** TYPES
 
   typedstruct enforce: true do
-    rect_fields()
     field :spec, Spec.t
-    field :image_spec, Image.Spec.t
-    field :mounts, [Mount.t()]
   end
-
-  # *** *******************************
-  # *** CONSTRUCTORS
-
-  # *** *******************************
-  # *** CONVERTERS
-
-  def mount_position(%__MODULE__{mounts: mounts}, mount_id) do
-    mounts
-    |> IdList.fetch!(mount_id)
-    |> position_new
-  end
-
-  def mounts(%__MODULE__{mounts: mounts}), do: mounts
 
 end
--- lib/dreadnought/sprite/builder.ex
+++ lib/dreadnought/sprite/builder.ex
@@ -1,7 +1,10 @@
 defmodule Dreadnought.Sprite.Builder do
 
-  def build(_sprite_spec) do
-    nil
+    use Dreadnought.Sprite.Spec
+  alias Dreadnought.Core.Sprites
+
+  def build({function_atom, arg} = sprite_spec) when is_sprite_spec(sprite_spec) do
+    apply(Sprites, function_atom, [arg])
   end
 
 end
