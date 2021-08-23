a883e68269fd3e15e67264ab275eb1756b42c300
Jonathan Chukinas (pop!_os) <chukinas@gmail.com>
2021-08-23 08:32:54 -0400

    Refactor, clean up SpriteComponent

--- lib/dreadnought_web/live/sprite_component.ex
+++ lib/dreadnought_web/live/sprite_component.ex
@@ -5,7 +5,7 @@ defmodule DreadnoughtWeb.SpriteComponent do
     use Dreadnought.PositionOrientationSize
   # TODO Spec functions should be aliased. Import only the guards
     use Dreadnought.Sprite.Spec
-  alias Dreadnought.Sprite.Builder
+  #alias Dreadnought.Sprite.Builder
   alias Dreadnought.Sprite.Improved
   alias DreadnoughtWeb.SvgView
 
@@ -22,11 +22,10 @@ defmodule DreadnoughtWeb.SpriteComponent do
     sprite_spec = posed_sprite.sprite_spec
     socket =
       socket
-      |> assign(pose: posed_sprite |> pose_from_map)
-      |> assign(sprite: posed_sprite.sprite_spec |> Builder.build)
+      #|> assign(pose: posed_sprite |> pose_from_map)
+      #|> assign(sprite: posed_sprite.sprite_spec |> Builder.build)
       |> assign(sprite_spec: sprite_spec)
-      |> assign(def_element: render_def_element(sprite_spec))
-      |> assign(use_element: render_use_element(sprite_spec))
+      #|> assign(use_element: _render_use_element(sprite_spec))
     {:ok, socket}
   end
 
@@ -36,9 +35,9 @@ defmodule DreadnoughtWeb.SpriteComponent do
     <%# TODO use dynamic values %>
     <svg id="sprite_component" viewbox="0 0 1000 1000" width="1000" height="1000" overflow="visible" >
       <defs>
-        <%= render_clippath_element(@sprite_spec) %>
+        <%= _render_clippath(@sprite_spec) %>
       </defs>
-      <%= render_image_element(@sprite_spec, @socket) %>
+      <%= _render_image_element(@sprite_spec, @socket) %>
     </svg>
     """
   end
@@ -46,20 +45,20 @@ defmodule DreadnoughtWeb.SpriteComponent do
   # *** *******************************
   # *** SPRITE.SPEC CONVERTERS
 
-  def render_def_element(sprite_spec) when is_sprite_spec(sprite_spec) do
-    sprite = Improved.from_sprite_spec(sprite_spec)
-    SvgView.render_polygon(Improved.coords(sprite))
-  end
+  #defp render_def_element(sprite_spec) when is_sprite_spec(sprite_spec) do
+  #  sprite = Improved.from_sprite_spec(sprite_spec)
+  #  SvgView.render_polygon(Improved.coords(sprite))
+  #end
 
-  def render_use_element(sprite_spec) when is_sprite_spec(sprite_spec) do
-    sprite_spec
-    |> element_id
-    |> SvgView.render_use
-  end
+  #defp _render_use_element(sprite_spec) when is_sprite_spec(sprite_spec) do
+  #  sprite_spec
+  #  |> _element_id
+  #  |> SvgView.render_use
+  #end
 
-  def element_id({func_name, arg} = sprite_spec) when is_sprite_spec(sprite_spec), do: "sprite-shape-#{func_name}-#{arg}"
+  defp _element_id({func_name, arg} = sprite_spec) when is_sprite_spec(sprite_spec), do: "sprite-shape-#{func_name}-#{arg}"
 
-  def render_image_element(sprite_spec, socket) when is_sprite_spec(sprite_spec) do
+  defp _render_image_element(sprite_spec, socket) when is_sprite_spec(sprite_spec) do
     improved_sprite = Improved.from_sprite_spec(sprite_spec)
     href = Routes.static_path(socket, Improved.image_path(improved_sprite))
     size = Improved.image_size(improved_sprite)
@@ -68,20 +67,20 @@ defmodule DreadnoughtWeb.SpriteComponent do
       #opacity: 0.5,
       x: position.x,
       y: position.y,
-      clip_path: "url(##{element_id(sprite_spec)})"
+      clip_path: "url(##{_element_id(sprite_spec)})"
     )
   end
 
-  def render_clippath_element(sprite_spec) when is_sprite_spec(sprite_spec) do
+  defp _render_clippath(sprite_spec) when is_sprite_spec(sprite_spec) do
     improved_sprite = Improved.from_sprite_spec(sprite_spec)
-    id = element_id(sprite_spec)
+    id = _element_id(sprite_spec)
     coords = Improved.coords(improved_sprite)
     SvgView.render_clippath(id, coords)
   end
 
-  def sprite(sprite_spec) do
-    Builder.build(sprite_spec)
-    |> IOP.inspect(__MODULE__)
-  end
+  #defp sprite(sprite_spec) do
+  #  Builder.build(sprite_spec)
+  #  |> IOP.inspect(__MODULE__)
+  #end
 
 end
