b911ac3106ff4dc1485a80520a0edc45b8ca92b9
Jonathan Chukinas (system76) <chukinas@gmail.com>
2021-11-29 21:24:54 -0500

    GG-250: Add ExDoc
    add ex_doc
    
    group modules in docs
    
    Move doc to top level
    
    Add logo
    
    add nesting and ignore some apps

--- apps/statechart/mix.exs
+++ apps/statechart/mix.exs
@@ -22,7 +22,7 @@ defmodule Statechart.MixProject do
     ]
   end
 
-  defp elixirc_paths(:dev), do: ["lib", "test/support"]
+  #defp elixirc_paths(:dev), do: ["lib", "test/support"]
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
