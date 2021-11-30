3c131b01c150d9d223345ca9b556cb4bf58fc244
Jonathan Chukinas (system76) <chukinas@gmail.com>
2021-11-29 21:24:54 -0500

    GG-250: Add ExDoc
    add ex_doc
    
    group modules in docs
    
    Move doc to top level
    
    Add logo
    
    add nesting and ignore some apps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Dreadnought.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      # ExDoc:
+      name: "Gratuitous Greyhound",
+      source_url: "https://github.com/jonathanchukinas/gratuitous_greyhound",
+      docs: docs()
     ]
   end
 
@@ -15,6 +19,7 @@ defmodule Dreadnought.Umbrella.MixProject do
   # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
     [
+      {:ex_doc, "~> 0.26", only: :dev, runtime: false},
       {:dialyxir, "~>1.1", only: [:dev], runtime: false}
     ]
   end
@@ -32,6 +37,43 @@ defmodule Dreadnought.Umbrella.MixProject do
     [
       # run `mix setup` in all child apps
       setup: ["cmd mix setup"]
+    ]
+  end
+
+  defp docs do
+    # https://hexdocs.pm/ex_doc/readme.html
+    [
+      groups_for_modules: [
+        Statechart: [~r/Statechart/],
+        Spatial: [Spatial],
+        "Linear Algebra": [~r/Spatial.LinearAlgebra/],
+        "Collision Detection": [~r/Spatial.Collide/],
+        "Position, Orientation, and Size": [~r/Spatial.PositionOrientationSize/],
+        "Geometry": [~r/Spatial.Geometry/],
+        "Math": [Spatial.Math],
+        "Modules in Need of a Better Home": [
+          ~r/Dreadnought/,
+          Spatial.BoundingRect,
+          Spatial.TypedStruct,
+          Spatial.Vector
+        ],
+      ],
+      logo: "docs_logo.jpg",
+      formatters: ["html"],
+      authors: ["Jonathan Chukinas"],
+      source_ref: "master",
+      #source_url_pattern: "https://github.com/jonathanchukinas/gratuitous_greyhound/blob/master/%{path}#L%{line}"
+      # https://hexdocs.pm/ex_doc/Mix.Tasks.Docs.html#module-nesting
+      nest_modules_by_prefix: [
+        Statechart,
+        Spatial,
+        SunsCore
+      ],
+      ignore_apps: [
+        :collision_detection,
+        :dreadnought,
+        :dreadnought_web
+      ]
     ]
   end
 end
